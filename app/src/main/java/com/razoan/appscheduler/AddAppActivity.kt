f5f644cc7a9281779babc9e4f10a3bbb0f9ff2c7
razoankabir <razoan@sheba.xyz>
2022-03-16 15:27:34 +0600

    Homepage and App schedule page view ready

---
+++ app/src/main/java/com/razoan/appscheduler/AddAppActivity.kt
@@ -0,0 +1,17 @@
+package com.razoan.appscheduler
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_app)
+        initListener()
+    }
+
+    private fun initListener() {
+
+
+    }
+}
